d1058b6b1469ffd9b1a3395f3781fee692a6e1ed
GioF_71 <giovanni.fulco@gmail.com>
2023-03-27 16:04:19 +0200

    Simplified

--- README.md
+++ README.md
@@ -6,24 +6,15 @@ Use the included script to build the image:
 
 `./build.sh`
 
-## Install Roon Bridge
+# Run
 
-Create a `roon` directory, maybe in the repo itself, and run:
-
-```
-docker run --rm \
-    --name roon-bridge \
-    -it -v ./roon:/roon \
-    --entrypoint=/app/bin/install-bridge.sh \
-    giof71/roon-bridge:local-bullseye
-```
-
-## Run
+Use the `roon` directory in the repo itself, and run:
 
 ```
 docker run --rm \
+    -it \
     --name roon-bridge \
-    -it -v ./roon:/roon \
+    -v ./roon:/roon \
     --network host \
     --device /dev/snd \
     giof71/roon-bridge:local-bullseye
--- app/bin/run-bridge.sh
+++ app/bin/run-bridge.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+echo "Install bridge"
+
+cd /roon
+
+if [[ ! -d "RoonBridge" ]]; then
+    URL_DIR="https://download.roonlabs.net/builds/"
+    declare -A file_dict
+    file_dict[x86_64]=RoonBridge_linuxx64.tar.bz2
+    file_dict[armv7l]=RoonBridge_linuxarmv7hf.tar.bz2
+    file_dict[aarch64]=RoonBridge_linuxarmv8.tar.bz2
+    ARCH=`uname -m`
+    FILENAME=${file_dict["${ARCH}"]}
+    FILE="/roon/$FILENAME"
+    curl -O "$URL_DIR/$FILENAME"
+    tar -xf $FILE
+    echo "Installation terminated"
+else
+    echo "Installation already exists"
+fi
+
 echo "Start bridge"
 
 cd /roon/RoonBridge
